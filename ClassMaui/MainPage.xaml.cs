91bc5817a429621db1535721117cfcca8ceaeeeb
paolo <paolo@DESKTOP-DD2FLAC>
2024-03-02 19:18:43 -0700

    Add project files.

---
+++ ClassMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClassMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ClassMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
